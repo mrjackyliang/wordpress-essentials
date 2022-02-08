8706256f682f83265b8917bdc558d2ebacc87c96
Jacky Liang <coolboii@me.com>
2022-02-08 18:24:55 -0500

    Fixed mail replacement issue
    ### FIXED
    - Mail from address and name being replaced regardless of other plugin settings.

--- features/white-label/mail.php
+++ features/white-label/mail.php
@@ -18,36 +18,55 @@ class Mail {
 	 */
 	function __construct() {
 		// Replace default email address.
-		add_filter( 'wp_mail_from', array( $this, 'replace_default_email_address' ) );
+		add_filter( 'wp_mail_from', array( $this, 'replace_default_email_address' ), 10, 1 );
 
 		// Replace default email name.
-		add_filter( 'wp_mail_from_name', array( $this, 'replace_default_email_name' ) );
+		add_filter( 'wp_mail_from_name', array( $this, 'replace_default_email_name' ), 10, 1 );
 	}
 
 	/**
 	 * Replace default email address.
 	 *
+	 * @param string $from_email Email address to send from.
+	 *
+	 * @return false|mixed|void
+	 *
 	 * @since 1.0.0
 	 */
-	function replace_default_email_address() {
-		if ( is_multisite() ) {
-			return get_site_option( 'admin_email' );
+	function replace_default_email_address( string $from_email ) {
+		$site_name   = wp_parse_url( network_home_url(), PHP_URL_HOST );
+		$domain_name = ( str_starts_with( $site_name, 'www.' ) ) ? substr( $site_name, 4 ) : $site_name;
+
+		if ( $from_email === 'wordpress@' . $domain_name ) {
+			if ( is_multisite() ) {
+				return get_site_option( 'admin_email' );
+			}
+
+			return get_option( 'admin_email' );
 		}
 
-		return get_option( 'admin_email' );
+		return $from_email;
 	}
 
 	/**
 	 * Replace default email name.
 	 *
+	 * @param string $from_name Name associated with the "from" email address.
+	 *
+	 * @return false|mixed|void
+	 *
 	 * @since 1.0.0
 	 */
-	function replace_default_email_name() {
-		if ( is_multisite() ) {
-			return get_site_option( 'site_name' );
+	function replace_default_email_name( string $from_name ) {
+		if ( $from_name === 'WordPress' ) {
+			if ( is_multisite() ) {
+				return get_site_option( 'site_name' );
+			}
+
+			return get_option( 'blogname' );
 		}
 
-		return get_option( 'blogname' );
+		return $from_name;
 	}
 }
 
